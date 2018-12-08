b7f1de14f22b9900491c34f42183be243c0369c3
Jie Bao <645372879@qq.com>
2018-12-08 16:23:02 +0000

    LoginTest

--- cypress/integration/addgood.spec.js
+++ cypress/integration/addgood.spec.js
@@ -1,6 +1,22 @@
 describe("Add Good page", () => {
 
   beforeEach(() => {
+    cy.request('https://shoppingwebapp.herokuapp.com/goods/')
+      .its('body')
+      .then( (goods) => {
+        goods.forEach( (element) => {
+          cy.request('DELETE',
+            'https://shoppingwebapp.herokuapp.com/goods/' + element._id)
+        });
+      })
+    // Populate API's datastore
+    cy.fixture('goods')
+      .then((goods) => {
+        goods.forEach((good) => {
+          cy.request('POST',
+            'https://shoppingwebapp.herokuapp.com/goods/', good )
+        })
+      })
     cy.visit("/");
     // Click Donate navbar link
     cy.get('.navbar-nav:nth-child(1)').
@@ -12,18 +28,21 @@ describe("Add Good page", () => {
     cy.get('#goods_name').select('Coke')
     cy.get('.col-md-6').find(':nth-child(2)').find('.form__input').type(1009);
     cy.get('.col-md-6').find(':nth-child(3)').find('.form__input').type('1200');
-    cy.get('.col-md-6').find(':nth-child(4)').find('.form__input').type('2');
+    cy.get('.col-md-6').find(':nth-child(4)').find('.form__input').type(2);
     cy.contains('Thanks for add Good!').should('not.exist');
     cy.get('.error').should('not.exist')
     cy.get('.col-md-6').find(':nth-child(5)').find('.btn').click();
     cy.contains('Thanks for add Good!').should('exist');
+    cy.get('.col-md-6').find(':nth-child(6)').find('.btn').click()
+    cy.url().should('include','/goods' )
+    cy.get('tbody').find('tr').should('have.length', 7)
   });
   it("shows error messages for incomplete form fields", () => {
     cy.get('.col-md-6').find(':nth-child(6)').find('.btn').click()
     cy.contains('Please Fill in the Form Correctly.').should('exist');
     cy.get('.error').contains('ID')
     cy.get('.col-md-6').find(':nth-child(2)').find('.form__input').type(1010);
-    cy.get('.col-md-6').find(':nth-child(3)').find('.form__input').type(1200);
+    cy.get('.col-md-6').find(':nth-child(3)').find('.form__input').type('1200');
     cy.get('.col-md-6').find(':nth-child(4)').find('.form__input').type(3);
     cy.get('.col-md-6').find(':nth-child(5)').find('.btn').click()
     cy.contains('Thanks for add Good!').should('exist');
